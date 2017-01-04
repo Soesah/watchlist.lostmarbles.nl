3556929254b4c8ede05985e1543299a0199fc58a
Carl Giesberts <carl@front-crafter.nl>
2017-01-04 10:07:56 +0100

    Added actors to game model

--- app/list/services/game-model.js
+++ app/list/services/game-model.js
@@ -2,13 +2,14 @@ angular.module('watchlistApp').factory('Game', ['BaseModel',
   function(BaseModel) {
 
   class Game extends BaseModel{
-    constructor({imdbId = null, name = null, year = null, score = null, played = false}) {
+    constructor({imdbId = null, name = null, year = null, score = null, actors = [], played = false}) {
       super();
       this.type = 3;
       this.imdbId = imdbId;
       this.name = name;
       this.year = year;
       this.score = score;
+      this.actors = actors;
       this.played = played;
     }
 
