e98b0b4f306d7b13bec241912a9e5663ed8a18a4
Carl Giesberts <carl@front-crafter.nl>
2017-11-26 15:36:59 +0100

    Version 2.0.4
    109ce64 Update view when changing route
    d72f8df Updated list
    7bd08af Franchises now also have a watched property based on their items
    28ea931 Updated list
    5eab063 Update version

--- src/components/list/WatchListFranchise.js
+++ src/components/list/WatchListFranchise.js
@@ -2,7 +2,7 @@ import WatchItemFactory from 'services/WatchItemFactory';
 import WatchListItem from 'components/list/WatchListItem';
 
 let WatchListFranchise = Vue.component('watch-list-franchise', {
-  template:`<li class="item franchise">
+  template:`<li :class="['item franchise', {'franchise-watched': item.watched}]">
               <router-link :to="'/view/franchise/' + this.item.path">
                 <h6 v-text="item.name"></h6>
                 <span class="bracketed" v-text="item.items.length"></span>
