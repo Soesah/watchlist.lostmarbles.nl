5cccf1b6c13a0018dc91440562557b6103b190ad
Soesah <carl@front-crafter.nl>
2017-11-30 17:02:54 +0100

    Hide apiKey

--- omdb.php
+++ omdb.php
@@ -2,7 +2,8 @@
   ini_set('display_errors', '0');
   header('Content-Encoding: utf-8');
 
-  $apiKey = '3e5351f0';
+  $apiKey = json_decode(file_get_contents('settings.json'), true)['apiKey'];
+
   $url = 'http://www.omdbapi.com/?'.$_SERVER['QUERY_STRING'].'&apiKey='.$apiKey;
 
   $data = json_decode(file_get_contents($url), true);
