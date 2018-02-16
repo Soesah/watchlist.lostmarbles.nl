6464f8df0fc67d1a89c2ccfe68df0f71186b6bac
Soesah <carl@front-crafter.nl>
2018-02-16 22:49:07 +0100

    Made watchlist item a functional component, to improve performance

--- src/components/list/WatchListItem.js
+++ src/components/list/WatchListItem.js
@@ -1,28 +1,39 @@
 import WatchItemFactory from 'services/WatchItemFactory';
 
 let WatchListItem = Vue.component('watch-list-item', {
-  template:`<li :class="['item movie', {'movie-watched': item.watched}]">
-              <router-link :to="'/view/' + item.path">
-                <h6 v-text="item.name"></h6>
-                <span class="bracketed" v-text="item.year"></span>
-                <i :class="'icon icon-' + icon" @click="toggleWatched"></i>
-              </router-link>
-            </li>`,
-  props: {
-    item: {
-      type: Object
-    }
-  },
-  computed: {
-    icon() {
-      return WatchItemFactory.getTypeName(this.item).toLowerCase();
-    }
-  },
-  methods: {
-    toggleWatched (evt) {
-      this.$store.dispatch('toggleWatched', this.item);
-      evt.preventDefault();
-      evt.stopPropagation();
-    }
+  functional: true,
+  render: function (createElement, context) {
+    const item = context.props.item;
+
+    return createElement('li', {
+      class: [
+        'item movie',
+        {'movie-watched': item.watched}
+      ]
+    }, [
+      createElement('router-link', {
+        props: {
+          to: '/view/' + item.path
+        }
+      }, [
+        createElement('h6', item.name),
+        createElement('span', {
+          class: 'bracketed',
+          domProps: {
+            innerHTML: item.year
+          }
+        }),
+        createElement('i',{
+          class: 'icon icon-' + WatchItemFactory.getTypeName(item).toLowerCase(),
+          on: {
+            click: (evt) => {
+              context.parent.$store.dispatch('toggleWatched', item);
+              evt.preventDefault();
+              evt.stopPropagation();
+            }
+          }
+        })
+      ])
+    ]);
   }
 });
