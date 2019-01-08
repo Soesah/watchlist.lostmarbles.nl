fe396ee80175e26fd648c11be3d60c36e0f6e5c0
Carl Giesberts <carl@front-crafter.nl>
2019-01-08 12:35:52 +0100

    Implemented list api

--- api/system/systemapi.go
+++ api/system/systemapi.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Soesah/watchlist.lostmarbles.nl/api"
 	"github.com/Soesah/watchlist.lostmarbles.nl/api/models"
+	"github.com/Soesah/watchlist.lostmarbles.nl/api/watchlist"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 )
@@ -128,91 +129,7 @@ func ImportData(items []models.WatchlistItem, r *http.Request) ([]models.Watchli
 
 // ExportData is used to export all items
 func ExportData(r *http.Request) ([]interface{}, error) {
-
-	ctx := appengine.NewContext(r)
-
-	var items []interface{}
-
-	var movies []models.Movie
-	var seriesData []models.SeriesData
-	var seasonsData []models.SeasonData
-	var episodes []models.Episode
-	var documentaries []models.Documentary
-	var games []models.Game
-	var franchises []models.Franchise
-
-	// movies
-	q := datastore.NewQuery(api.MovieKind)
-	_, err := q.GetAll(ctx, &movies)
-	if err != nil {
-		return nil, err
-	}
-	for _, movie := range movies {
-		items = append(items, movie)
-	}
-
-	// series
-	q = datastore.NewQuery(api.SeriesKind)
-	_, err = q.GetAll(ctx, &seriesData)
-	if err != nil {
-		return nil, err
-	}
-
-	q = datastore.NewQuery(api.SeasonKind)
-	_, err = q.GetAll(ctx, &seasonsData)
-	if err != nil {
-		return nil, err
-	}
-
-	q = datastore.NewQuery(api.EpisodeKind)
-	_, err = q.GetAll(ctx, &episodes)
-	if err != nil {
-		return nil, err
-	}
-
-	// add the episodes to the seasons
-	var seasons []models.Season
-	for _, seasonData := range seasonsData {
-		season := seasonData.GetSeason(episodes)
-		seasons = append(seasons, season)
-	}
-	// add the seasons to the series
-	for _, serialData := range seriesData {
-		serial := serialData.GetSeries(seasons)
-		items = append(items, serial)
-	}
-
-	// documentaries
-	q = datastore.NewQuery(api.DocumentaryKind)
-	_, err = q.GetAll(ctx, &documentaries)
-	if err != nil {
-		return nil, err
-	}
-	for _, documentary := range documentaries {
-		items = append(items, documentary)
-	}
-
-	// games
-	q = datastore.NewQuery(api.GameKind)
-	_, err = q.GetAll(ctx, &games)
-	if err != nil {
-		return nil, err
-	}
-	for _, game := range games {
-		items = append(items, game)
-	}
-
-	// franchises
-	q = datastore.NewQuery(api.FranchiseKind)
-	_, err = q.GetAll(ctx, &franchises)
-	if err != nil {
-		return nil, err
-	}
-	for _, franchise := range franchises {
-		items = append(items, franchise)
-	}
-
-	return items, nil
+	return watchlist.GetWatchList(r)
 }
 
 // ClearAllItems is used to delete all items
--- api/watchlist/watchlistapi.go
+++ api/watchlist/watchlistapi.go
@@ -1,14 +1,112 @@
 package watchlist
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/Soesah/watchlist.lostmarbles.nl/api"
 	"github.com/Soesah/watchlist.lostmarbles.nl/api/models"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 )
 
 // GetWatchList returns the whole watch list
 func GetWatchList(r *http.Request) ([]interface{}, error) {
 	var list []interface{}
+
+	var movies []models.Movie
+	var seriesData []models.SeriesData
+	var seasonsData []models.SeasonData
+	var episodes []models.Episode
+	var documentaries []models.Documentary
+	var games []models.Game
+	var franchises []models.Franchise
+
+	ctx := appengine.NewContext(r)
+
+	options := datastore.TransactionOptions{
+		XG: true,
+	}
+
+	err := datastore.RunInTransaction(ctx, func(ct context.Context) error {
+
+		// movies
+		q := datastore.NewQuery(api.MovieKind)
+		_, err := q.GetAll(ctx, &movies)
+		if err != nil {
+			return err
+		}
+		for _, movie := range movies {
+			list = append(list, movie)
+		}
+
+		// series
+		q = datastore.NewQuery(api.SeriesKind)
+		_, err = q.GetAll(ctx, &seriesData)
+		if err != nil {
+			return err
+		}
+
+		q = datastore.NewQuery(api.SeasonKind)
+		_, err = q.GetAll(ctx, &seasonsData)
+		if err != nil {
+			return err
+		}
+
+		q = datastore.NewQuery(api.EpisodeKind)
+		_, err = q.GetAll(ctx, &episodes)
+		if err != nil {
+			return err
+		}
+
+		// add the episodes to the seasons
+		var seasons []models.Season
+		for _, seasonData := range seasonsData {
+			season := seasonData.GetSeason(episodes)
+			seasons = append(seasons, season)
+		}
+		// add the seasons to the series
+		for _, serialData := range seriesData {
+			serial := serialData.GetSeries(seasons)
+			list = append(list, serial)
+		}
+
+		// documentaries
+		q = datastore.NewQuery(api.DocumentaryKind)
+		_, err = q.GetAll(ctx, &documentaries)
+		if err != nil {
+			return err
+		}
+		for _, documentary := range documentaries {
+			list = append(list, documentary)
+		}
+
+		// games
+		q = datastore.NewQuery(api.GameKind)
+		_, err = q.GetAll(ctx, &games)
+		if err != nil {
+			return err
+		}
+		for _, game := range games {
+			list = append(list, game)
+		}
+
+		// franchises
+		q = datastore.NewQuery(api.FranchiseKind)
+		_, err = q.GetAll(ctx, &franchises)
+		if err != nil {
+			return err
+		}
+		for _, franchise := range franchises {
+			list = append(list, franchise)
+		}
+
+		return nil
+	}, &options)
+
+	if err != nil {
+		return nil, err
+	}
 
 	return list, nil
 }
