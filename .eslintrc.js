2c4b4d7a0be8a3c2238ebb5c60020a68701248b7
Carl Giesberts <carl@front-crafter.nl>
2019-10-26 09:40:17 +0200

    Updated packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  extends: ['plugin:vue/essential', '@vue/typescript'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': {
-      quotes: [
-        'error',
-        'single',
-        { avoidEscape: true, allowTemplateLiterals: true }
-      ]
-    }
-  },
-  parserOptions: {
-    parser: 'typescript-eslint-parser'
+    'prettier/prettier': 0
   }
 };
